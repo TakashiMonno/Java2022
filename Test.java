a28dc1e7425fb8e662fe56a915b0421d61356739
TakashiMonno <sesirou@gmail.com>
2022-06-13 01:52:48 +0900

    Test commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+package Java2022;
+
+class Main {
+    public static void main(String[] args) {
+      String msg = "";
+      msg += "Hello ";
+      msg += "World!";
+      System.out.println(msg);
+    }
+  }
